ede623255cd1d359f300f5ae0050035539005310
Wassim Triki <wsmtriki@gmail.com>
2023-02-25 14:34:11 +0100

    add reusable button variants

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -26,18 +26,8 @@ export const Hero = () => {
         maintenance of wired telecommunication networks.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button
-          className={
-            'bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900 leading-normal cursor-pointer'
-          }
-        >
-          Get in touch
-        </Button>
-
-        <Link
-          to="#"
-          className="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300 leading-normal cursor-pointer"
-        >
+        <Button variant="secondary">Get in touch</Button>
+        <Button variant="tertiary">
           <svg
             aria-hidden="true"
             className="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
@@ -45,7 +35,7 @@ export const Hero = () => {
             <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z"></path>
           </svg>
           <span className="ml-3">Watch video</span>
-        </Link>
+        </Button>
       </div>
     </Container>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 import { useNavContext } from '../context/store';
 import { navLinks } from '../data/nav-links';
+import { Button } from './Button';
 import { NavButton } from './NavButton';
 import { NavLink } from './NavLink';
 import { NavListMobile } from './NavListMobile';
@@ -24,12 +25,7 @@ export const Nav = () => {
         </div>
       </div>
       <div className="flex items-center gap-x-5 md:gap-x-8">
-        <Link
-          to="#"
-          className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600 cursor-pointer leading-normal"
-        >
-          Get in touch
-        </Link>
+        <Button to="#">Get in touch</Button>
         <NavButton />
         {expandNav ? (
           <>
