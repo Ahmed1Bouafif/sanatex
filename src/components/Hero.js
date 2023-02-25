a8e212f1722df7dd380b2d2c7f8314705212af40
Wassim Triki <wsmtriki@gmail.com>
2023-02-25 14:46:57 +0100

    add trunsted by in header

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 import { Button } from './Button';
 import Container from './Container';
+import { TrustedByItem } from './TrustedByItem';
 
 export const Hero = () => {
   return (
@@ -36,6 +37,31 @@ export const Hero = () => {
           </svg>
           <span className="ml-3">Watch video</span>
         </Button>
+      </div>
+
+      <div className="mt-36 lg:mt-44">
+        <h3 className="font-display text-base text-slate-900">
+          Trusted by these six companies so far
+        </h3>
+        <ul
+          role="list"
+          className="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
+        >
+          <li>
+            <ul className="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0">
+              <TrustedByItem />
+              <TrustedByItem />
+              <TrustedByItem />
+            </ul>
+          </li>
+          <li>
+            <ul className="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0">
+              <TrustedByItem />
+              <TrustedByItem />
+              <TrustedByItem />
+            </ul>
+          </li>
+        </ul>
       </div>
     </Container>
   );
---
+++ src/components/TrustedByItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TrustedByItem = () => {
+  return (
+    <li className="flex">
+      <img src="https://salient.tailwindui.com/_next/static/media/transistor.7274e6c3.svg" />
+    </li>
+  );
+};
