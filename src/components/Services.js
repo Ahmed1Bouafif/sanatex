60276747720297807d2e2fdbd5a9df990cf546c1
Wassim Triki <wsmtriki@gmail.com>
2023-03-08 13:36:16 +0100

    add animation to mobile nav

--- src/components/Services.js
+++ src/components/Services.js
@@ -54,16 +54,21 @@ export const Services = () => {
   return (
     <Section className>
       <Container className="flex flex-col gap-10">
-        <h2 className="text-center md:text-left font-display text-4xl tracking-tight sm:text-5xl md:text-6xl">
+        <motion.h2
+          initial={{ x: -200, opacity: 0 }}
+          whileInView={{ x: 1, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-center md:text-left font-display text-4xl tracking-tight sm:text-5xl md:text-6xl"
+        >
           Services
-        </h2>
+        </motion.h2>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-4 ">
           {services.map((service, idx) => (
             <motion.div
               initial={{ opacity: 0, scale: 0 }}
               whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.5, delay: idx / 20 }}
+              transition={{ duration: 0.4, delay: idx / 20 }}
             >
               <ServiceCard {...service} />
             </motion.div>
