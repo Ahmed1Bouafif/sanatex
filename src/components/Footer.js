d41119308027c4cc10c3eebfeba552076d2f5f0e
Wassim Triki <wsmtriki@gmail.com>
2023-03-14 07:31:55 +0100

    fix card gradient

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,23 @@
-import { Link } from "gatsby"
-import React from "react"
-import Container from "./Container"
-import { FooterCopyright } from "./FooterCopyright"
+import { Link } from 'gatsby';
+import React from 'react';
+import Container from './Container';
+import { FooterCopyright } from './FooterCopyright';
 
 export const Footer = () => {
   const links = [
     {
-      name: "Publication Details",
-      pathname: "/publication-details",
+      name: 'Publication Details',
+      pathname: '/publication-details',
     },
     {
-      name: "Privacy Policy",
-      pathname: "/privacy-policy",
+      name: 'Privacy Policy',
+      pathname: '/privacy-policy',
     },
     {
-      name: "LinkedIn",
-      pathname: "#",
+      name: 'LinkedIn',
+      pathname: '#',
     },
-  ]
+  ];
   return (
     <footer className="h-screen flex flex-col">
       <div class="flex flex-col gap-40 justify-between px-4 pt-16  mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
@@ -25,13 +25,22 @@ export const Footer = () => {
           <div class="md:flex flex flex-col content-center max-w-lg text-center gap-2 md:justify-between md:items-center">
             <div>
               <h2 class="text-3xl font-bold mb-6">Contact Us</h2>
-              <p class="text-lg mb-4">MultiNet Communication est la première adresse pour les solutions globales dans les installations suisses de télécommunication.</p>
+              <p class="text-lg mb-4">
+                MultiNet Communication is the first address for integrated
+                solutions in Swiss telecommunications systems.
+              </p>
             </div>
             <div class="mt-8 md:mt-0 flex justify-center md:justify-end">
-              <a href="tel:1234567890" class="text-lg px-8 py-3 rounded-full border border-blue-400 text-blue-400 hover:bg-blue-400 hover:text-white transition-colors duration-300 mr-4">
+              <a
+                href="tel:1234567890"
+                class="text-lg px-8 py-3 rounded-full border border-blue-400 text-blue-400 hover:bg-blue-400 hover:text-white transition-colors duration-300 mr-4"
+              >
                 Ecriver-nous
               </a>
-              <a href="mailto:contact@mynetworkcompany.com" class="text-lg px-8 py-3 rounded-full bg-blue-400 text-white hover:bg-blue-300 transition-colors duration-300">
+              <a
+                href="mailto:contact@mynetworkcompany.com"
+                class="text-lg px-8 py-3 rounded-full bg-blue-400 text-white hover:bg-blue-300 transition-colors duration-300"
+              >
                 Poster A Pourvoir
               </a>
             </div>
@@ -39,18 +48,40 @@ export const Footer = () => {
         </div>
         <div class="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
           <div class="md:max-w-md lg:col-span-2">
-            <a href="/" aria-label="Go home" title="Company" class="inline-flex items-center">
-              <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
+            <a
+              href="/"
+              aria-label="Go home"
+              title="Company"
+              class="inline-flex items-center"
+            >
+              <svg
+                class="w-8 text-deep-purple-accent-400"
+                viewBox="0 0 24 24"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
+                stroke="currentColor"
+                fill="none"
+              >
                 <rect x="3" y="1" width="7" height="12"></rect>
                 <rect x="3" y="17" width="7" height="6"></rect>
                 <rect x="14" y="1" width="7" height="6"></rect>
                 <rect x="14" y="11" width="7" height="12"></rect>
               </svg>
-              <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">SanateX</span>
+              <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                SanateX
+              </span>
             </a>
             <div class="mt-4 lg:max-w-sm">
-              <p class="text-sm text-gray-800">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-              <p class="mt-4 text-sm text-gray-800">Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+              <p class="text-sm text-gray-800">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam.
+              </p>
+              <p class="mt-4 text-sm text-gray-800">
+                Eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                beatae vitae dicta sunt explicabo.
+              </p>
             </div>
           </div>
           <div class="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
@@ -58,22 +89,34 @@ export const Footer = () => {
               <p class="font-semibold tracking-wide text-gray-800">Category</p>
               <ul class="mt-2 space-y-2">
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     News
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     World
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Games
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     References
                   </a>
                 </li>
@@ -83,27 +126,42 @@ export const Footer = () => {
               <p class="font-semibold tracking-wide text-gray-800">Business</p>
               <ul class="mt-2 space-y-2">
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Web
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     eCommerce
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Business
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Entertainment
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Portfolio
                   </a>
                 </li>
@@ -113,27 +171,42 @@ export const Footer = () => {
               <p class="font-semibold tracking-wide text-gray-800">Apples</p>
               <ul class="mt-2 space-y-2">
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Media
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Brochure
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Nonprofit
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Educational
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Projects
                   </a>
                 </li>
@@ -143,22 +216,34 @@ export const Footer = () => {
               <p class="font-semibold tracking-wide text-gray-800">Cherry</p>
               <ul class="mt-2 space-y-2">
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Infopreneur
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Personal
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Wiki
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                  <a
+                    href="/"
+                    class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                  >
                     Forum
                   </a>
                 </li>
@@ -169,5 +254,5 @@ export const Footer = () => {
       </div>
       <FooterCopyright />
     </footer>
-  )
-}
+  );
+};
--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -11,7 +11,7 @@ import { motion } from 'framer-motion';
 export const ServiceCard = ({ title, icon: Icon }) => {
   return (
     <motion.div className="relative bg-blue-40s0  flex flex-col lg:p-8 p-16 gap-10 hover: transition-all rounded-[4px] overflow-hidden group cursor-pointer justify-between h-full min-w-[250px]">
-      <div className="absolute h-full w-full top-0 left-0 bg-gradient-to-br from-blue-500 to-purple-500 group-hover:opacity-100 opacity-0 z-0 transition-all "></div>
+      <div className="service-card absolute h-full w-full top-0 left-0 bg-gradient-to-br  bg-blue-500 group-hover:opacity-100 opacity-0 z-0 transition-all "></div>
       <div className="flex justify-center lg:justify-start z-10">
         <Icon className=" w-1/3 group-hover:stroke-white text-black" />
       </div>
