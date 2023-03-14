7bf05b72c85460c1671e6047591a01d782c22e10
Wassim Triki <wsmtriki@gmail.com>
2023-03-14 17:40:46 +0100

    add 2 forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import { toUpper } from 'lodash';
+import React, { useState } from 'react';
 import Container from './Container';
 import FormInput from './FormInput';
 import { Section } from './Section';
 
 const Contact = () => {
-  const inputs = [
+  const openPosInputs = [
+    {
+      label: 'gender (Mr/Mrs)',
+      name: 'contact-2-gender',
+      type: 'select',
+      required: false,
+    },
+    {
+      label: 'Name',
+      name: 'contact-2-name',
+      type: 'text',
+      required: true,
+    },
+    {
+      label: 'email',
+      name: 'contact-2-email',
+      type: 'email',
+      required: true,
+    },
+    {
+      label: 'Phone',
+      name: 'contact-2-phone',
+      type: 'text',
+      required: false,
+    },
+    {
+      label: 'CV',
+      name: 'contact-2-cv',
+      type: 'file',
+      required: true,
+    },
+    {
+      label: 'cover letter',
+      name: 'contact-2-cl',
+      type: 'file',
+      required: true,
+    },
+    {
+      label: 'Message',
+      name: 'contact-2-message',
+      type: 'textarea',
+      required: false,
+    },
+  ];
+
+  const writeUsInputs = [
     {
       label: 'First name',
       name: 'contact-1-first',
@@ -48,6 +94,21 @@ const Contact = () => {
       required: false,
     },
   ];
+
+  const forms = [writeUsInputs, openPosInputs];
+  const [activeTab, setActiveTab] = useState(0);
+  const tabs = [
+    {
+      label: 'write us',
+    },
+    {
+      label: 'open positions',
+    },
+  ];
+
+  const handleChangeTab = (idx) => {
+    setActiveTab(idx);
+  };
   return (
     <div className="bg-black text-white  py-20">
       <Container className="grid grid-cols-5 grid-rows-2">
@@ -56,10 +117,24 @@ const Contact = () => {
             Get in touch with us
           </p>
         </div>
-        <div className="bg-gray-6s00 row-span-2 col-span-2">
-          <form className="flex flex-col bg-grseen-500 px-16 gap-4">
-            <div className="flex flex-col gap-4  bg-rsed-300 items-center">
-              {inputs.map((input, idx) => (
+        <div className="bg-gray-6s00 row-span-2 flex flex-col gap-1 col-span-2 px-16">
+          <div className="flex w-full">
+            {tabs.map((tab, idx) => (
+              <button
+                onClick={(e) => handleChangeTab(idx)}
+                className={`w-full transition-all ${
+                  activeTab === idx
+                    ? 'text-black bg-white'
+                    : 'text-white bg-black'
+                }   font-semibold py-4 `}
+              >
+                {toUpper(tab.label)}
+              </button>
+            ))}
+          </div>
+          <form className="flex flex-col bg-grseen-500  gap-4">
+            <div className="flex flex-col gap-2  bg-rsed-300 items-center">
+              {forms[activeTab]?.map((input, idx) => (
                 <FormInput key={input.name} {...input} />
               ))}
             </div>
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,34 +1,55 @@
+import { capitalize } from 'lodash';
 import React from 'react';
 
 const FormInput = ({ label, name, type, required }) => {
   let field;
-  if (type === 'textarea') {
-    field = (
-      <textarea
-        placeholder={label}
-        className="form__field text-white resize-none scrollbar-hide"
-        name={label}
-        id={label}
-      />
-    );
-  } else {
-    field = (
-      <input
-        className="form__field "
-        type={type}
-        id={name}
-        name={name}
-        placeholder={label}
-        required={required}
-      />
-    );
+
+  switch (type) {
+    case 'textarea':
+      field = (
+        <textarea
+          placeholder={label}
+          className="form__field text-white resize-none scrollbar-hide"
+          name={name}
+          id={name}
+        />
+      );
+      break;
+    case 'select':
+      field = (
+        <select className="form__field" name={name} id={name}>
+          <option
+            className="text-[#9b9b9b]"
+            selected
+            disabled
+            hidden
+            value
+          ></option>
+          <option value="mr">Mr</option>
+          <option value="mrs">Mrs</option>
+        </select>
+      );
+      break;
+    default:
+      field = (
+        <input
+          className={`form__field`}
+          type={type}
+          id={name}
+          name={name}
+          placeholder={label}
+          required={required}
+        />
+      );
   }
+
   return (
     <div key={name} className="form__group field font-heading">
       {field}
       <label className="form__label font-heading" htmlFor={name}>
-        {required ? `${label}*` : label}
+        {required ? capitalize(`${label}*`) : capitalize(label)}
       </label>
+      <div className="w-full h-[2px] bg-slate-200"></div>
     </div>
   );
 };
