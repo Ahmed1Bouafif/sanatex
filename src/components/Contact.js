6ed74a0e6e5cf25da682b04086834af3cf161dd1
Wassim Triki <wsmtriki@gmail.com>
2023-03-14 19:35:26 +0100

    add form responsivity

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,14 +24,14 @@ const Contact = () => {
   };
   return (
     <div className="bg-black text-white  py-20">
-      <Container className="grid grid-cols-5 grid-rows-2">
-        <div className="bg-red-30s0 col-span-3">
-          <p className="text-white text-8xl font-heading">
+      <Container className="grid grid-cols-1 md:grid-cols-5 md:grid-rows-2">
+        <div className="bg-red-30s0 col-span-3 mb:4">
+          <p className="text-white text-center text-5xl md:text-left md:text-7xl lg:text-8xl font-heading ">
             Get in touch with us
           </p>
         </div>
-        <div className="bg-gray-6s00 row-span-2 flex flex-col gap-1 col-span-2 px-16">
-          <div className="flex w-full">
+        <div className="bg-gray-6s00 row-span-2 flex flex-col gap-1 col-span-2 md:px-16">
+          <div className="flex w-full mt-8 md:mt-0">
             {tabs.map((tab, idx) => (
               <button
                 key={tab.label}
@@ -40,7 +40,7 @@ const Contact = () => {
                   activeTab === idx
                     ? 'text-black bg-white'
                     : 'text-white bg-black'
-                }   font-semibold py-4 `}
+                }   font-semibold py-4  sm:text-sm md:text-sm lg:text-base `}
               >
                 {toUpper(tab.label)}
               </button>
@@ -49,7 +49,7 @@ const Contact = () => {
           {/* {forms[activeTab]()} */}
           {activeTab === 0 ? <WriteUsForm /> : <OpenPosForm />}
         </div>
-        <div className="bg-blue-30s0 col-span-3">
+        <div className="bg-blue-30s0 col-span-3 mt-8 md:mt-0">
           <div className="text-white h-full flex justify-center items-center">
             other contact info
           </div>
