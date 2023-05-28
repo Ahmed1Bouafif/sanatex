0ad8099a53dcb6f884e3331898176891c9a1a9c6
AhmedBouafif <ahmedbouafif500@gmail.com>
2023-05-28 23:55:06 +0100

    lastchangeslastmai

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,75 +1,47 @@
-import React, { useEffect } from 'react';
-import { ChevronDoubleDownIcon } from '@heroicons/react/24/outline';
-import { motion } from 'framer-motion';
-import { useTranslation } from 'react-i18next';
-import { capitalize } from 'lodash';
-import { Collapse } from 'react-collapse';
-import { useThemeContext } from '../context/store';
+import React, { useEffect } from "react"
+import { ChevronDoubleDownIcon } from "@heroicons/react/24/outline"
+import { motion } from "framer-motion"
+import { useTranslation } from "react-i18next"
+import { capitalize } from "lodash"
+import { Collapse } from "react-collapse"
+import { useThemeContext } from "../context/store"
 
-export const ServiceCard = ({
-  name,
-  title,
-  idx,
-  description,
-  subtitle,
-  icon: Icon,
-  setActiveService,
-  activeService,
-  setPrev,
-}) => {
-  const { t } = useTranslation();
-  const isActive = activeService && idx === activeService;
-  const { theme, setTheme } = useThemeContext();
+export const ServiceCard = ({ name, title, idx, description, subtitle, icon: Icon, setActiveService, activeService, setPrev }) => {
+  const { t } = useTranslation()
+  const isActive = activeService && idx === activeService
+  const { theme, setTheme } = useThemeContext()
 
   useEffect(() => {
     // if (isActive) console.log(idx, 'is active');
-  }, [activeService]);
+  }, [activeService])
   return (
     <>
       <motion.div
         onClick={(e) => {
           // console.log(idx, activeService);
-          console.log(description);
+          console.log(description)
           setActiveService((id) => {
             if (id) {
-              setPrev(id);
+              setPrev(id)
             }
-            return id === idx ? null : idx;
-          });
+            return id === idx ? null : idx
+          })
         }}
         className="relative  flex flex-col z-10 p-10 lsg:p-8 md:p-8 gap-6 hover: transition-all roundsed-[4px] overflow-hidden group cursor-pointer gsap-10 lg:justify-between h-full min-w-[240px] "
       >
-        <div
-          className={` absolute h-full w-full top-0 left-0 group-hover:opacity-100 opacity-0 z-0 transition-all service-card`}
-        ></div>
-        {isActive ? (
-          <div
-            className={` absolute h-full w-full top-0 left-0 bg-black dark:bg-white z-0 transition-all}`}
-          ></div>
-        ) : (
-          ''
-        )}
+        <div className={` absolute h-full w-full top-0 left-0 group-hover:opacity-100 opacity-0 z-0 transition-all service-card`}></div>
+        {isActive ? <div className={` absolute h-full w-full top-0 left-0 bg-black dark:bg-white z-0 transition-all}`}></div> : ""}
 
         {/* <div className="flex justify-center lg:justify-start z-10">
           {/* <img src={Icon} alt="" className="max-w-[100px] mx-auto" /> */}
         {/* <Icon className={`z-0 max-w-[100px] group-hover:stroke-white text-black transition-all ${isActive ? "stroke-white" : ""}`} /> */}
         {/* </div>  */}
         <div className="flex flex-col justify-center my-auto gap-5  sm:gap-2 z-10 font-semibold ">
-          <h3
-            className={`text-center mx-auto text-xl sm:text-lg text-black dark:text-white    group-hover:text-white texst-black ${
-              isActive ? 'text-white dark:!text-black' : ''
-            } `}
-          >
+          <h3 className={`text-center mx-auto text-xl sm:text-lg text-black dark:text-white    group-hover:text-white texst-black ${isActive ? "text-white dark:!text-black" : ""} `}>
             {/* {capitalize(t(name))} */}
             {name}
             <br />
-            <p
-              className={`text-center mx-auto font-normal text-base mt-5 text-black/80 dark:text-white/80    group-hover:text-white texst-black ${
-                isActive ? 'text-white/80 dark:!text-black/80' : ''
-              } `}
-            >
-              {subtitle}
-            </p>
+            <p className={`text-center mx-auto font-normal text-base mt-5 text-black/80 dark:text-white/80    group-hover:text-white texst-black ${isActive ? "text-white/80 dark:!text-black/80" : ""} `}>{subtitle} </p>
           </h3>
           {/* <button to="#" className="justify-center  flex items-center  text-black transition-all ">
             <h4 className={`group-hover:text-white text-black hidden lg:flex ${isActive ? "text-white" : ""}`}>Mehr lesen</h4>{" "}
@@ -82,12 +54,8 @@ export const ServiceCard = ({
       </motion.div>
 
       <Collapse isOpened={isActive}>
-        <div
-          className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading md:hidden`}
-        >
-          {description}
-        </div>
+        <div className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading md:hidden`}>{description}</div>
       </Collapse>
     </>
-  );
-};
+  )
+}
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,52 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { Section } from './Section';
-import { Link } from 'gatsby';
+import React, { useEffect, useState } from "react"
+import { Section } from "./Section"
+import { Link } from "gatsby"
 
-import { ServiceCard } from './ServiceCard';
+import { ServiceCard } from "./ServiceCard"
 
-import { motion } from 'framer-motion';
-import { services } from '../data/services';
-import { useLocation } from '@reach/router';
-import { Button2 } from './Button2';
-import { Collapse } from 'react-collapse';
+import { motion } from "framer-motion"
+import { services } from "../data/services"
+import { useLocation } from "@reach/router"
+import { Button2 } from "./Button2"
+import { Collapse } from "react-collapse"
 function groupArray(a, x) {
-  let b = [];
+  let b = []
   for (let i = 0; i < a.length; i += x) {
-    let group = [];
+    let group = []
     for (let j = i; j < i + x && j < a.length; j++) {
-      group.push(a[j]);
+      group.push(a[j])
     }
-    b.push(group);
+    b.push(group)
   }
-  return b;
+  return b
 }
 export const Services = () => {
-  const location = useLocation();
-  const isServicesPage = location.pathname.includes('/services');
-  const [activeService, setActiveService] = useState(null);
-  const [prev, setPrev] = useState(activeService);
+  const location = useLocation()
+  const isServicesPage = location.pathname.includes("/services")
+  const [activeService, setActiveService] = useState(null)
+  const [prev, setPrev] = useState(activeService)
 
   useEffect(() => {
-    console.log(activeService);
-  }, [activeService]);
+    console.log(activeService)
+  }, [activeService])
   return (
     <Section title="Dienstleistungen">
       <div className="flex flex-col">
         <div className="md:hidden flex flex-col">
           {services.map((service, idx) => (
-            <motion.div
-              key={service.title}
-              className="maxs-h-[300px]"
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.2, delay: idx / 20 }}
-            >
-              <ServiceCard
-                {...service}
-                setActiveService={setActiveService}
-                activeService={activeService}
-                setPrev={setPrev}
-              />
+            <motion.div key={service.title} className="maxs-h-[300px]" initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: idx / 20 }}>
+              <ServiceCard {...service} setActiveService={setActiveService} activeService={activeService} setPrev={setPrev} />
             </motion.div>
           ))}
         </div>
@@ -54,175 +43,75 @@ export const Services = () => {
         <div className="hidden md:flex lg:hidden flex-col ">
           <div className="flex justify-center">
             {services.slice(0, 2).map((service) => (
-              <motion.div
-                key={service.title}
-                className="min-h-[250px] flex-1 "
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: service.idx / 20 }}
-              >
-                <ServiceCard
-                  {...service}
-                  setActiveService={setActiveService}
-                  activeService={activeService}
-                  setPrev={setPrev}
-                />
+              <motion.div key={service.title} className="min-h-[250px] flex-1 " initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: service.idx / 20 }}>
+                <ServiceCard {...service} setActiveService={setActiveService} activeService={activeService} setPrev={setPrev} />
               </motion.div>
             ))}
           </div>
           <Collapse isOpened={activeService && activeService <= 2}>
-            <div
-              className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}
-            >
-              {services[activeService - 1]?.description ||
-                services[prev - 1]?.description}
-            </div>
+            <div className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}>{services[activeService - 1]?.description || services[prev - 1]?.description}</div>
           </Collapse>
           <div className="flex justify-center">
             {services.slice(2, 4).map((service) => (
-              <motion.div
-                key={service.title}
-                className="min-h-[250px] flex-1 "
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: service.idx / 20 }}
-              >
-                <ServiceCard
-                  {...service}
-                  setActiveService={setActiveService}
-                  activeService={activeService}
-                  setPrev={setPrev}
-                />
+              <motion.div key={service.title} className="min-h-[250px] flex-1 " initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: service.idx / 20 }}>
+                <ServiceCard {...service} setActiveService={setActiveService} activeService={activeService} setPrev={setPrev} />
               </motion.div>
             ))}
           </div>
-          <Collapse
-            isOpened={activeService && activeService > 2 && activeService <= 4}
-          >
-            <div
-              className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}
-            >
-              {services[activeService - 1]?.description ||
-                services[prev - 1]?.description}
-            </div>
+          <Collapse isOpened={activeService && activeService > 2 && activeService <= 4}>
+            <div className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}>{services[activeService - 1]?.description || services[prev - 1]?.description}</div>
           </Collapse>
           <div className="flex justify-center">
             {services.slice(4, 6).map((service) => (
-              <motion.div
-                key={service.title}
-                className="min-h-[250px] flex-1 "
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: service.idx / 20 }}
-              >
-                <ServiceCard
-                  {...service}
-                  setActiveService={setActiveService}
-                  activeService={activeService}
-                  setPrev={setPrev}
-                />
+              <motion.div key={service.title} className="min-h-[250px] flex-1 " initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: service.idx / 20 }}>
+                <ServiceCard {...service} setActiveService={setActiveService} activeService={activeService} setPrev={setPrev} />
               </motion.div>
             ))}
           </div>
-          <Collapse
-            isOpened={activeService && activeService > 4 && activeService <= 6}
-          >
-            <div
-              className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}
-            >
-              {services[activeService - 1]?.description ||
-                services[prev - 1]?.description}
-            </div>
+          <Collapse isOpened={activeService && activeService > 4 && activeService <= 6}>
+            <div className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}>{services[activeService - 1]?.description || services[prev - 1]?.description}</div>
           </Collapse>
           <div className="flex justify-center">
             {services.slice(6).map((service) => (
-              <motion.div
-                key={service.title}
-                className="min-h-[250px] flex-1 "
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: service.idx / 20 }}
-              >
-                <ServiceCard
-                  {...service}
-                  setActiveService={setActiveService}
-                  activeService={activeService}
-                  setPrev={setPrev}
-                />
+              <motion.div key={service.title} className="min-h-[250px] flex-1 " initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: service.idx / 20 }}>
+                <ServiceCard {...service} setActiveService={setActiveService} activeService={activeService} setPrev={setPrev} />
               </motion.div>
             ))}
           </div>
         </div>
         <Collapse isOpened={activeService && activeService > 6}>
-          <div
-            className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}
-          >
-            {services[activeService - 1]?.description ||
-              services[prev - 1]?.description}
-          </div>
+          <div className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden md:block lg:hidden`}>{services[activeService - 1]?.description || services[prev - 1]?.description}</div>
         </Collapse>
         <div className="hidden lg:flex lg:flex-col">
           <div className="flex justify-center">
-            {services.slice(0, 3).map((service) => (
-              <motion.div
-                key={service.title}
-                className="h-[250px] flex-1"
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: service.idx / 20 }}
-              >
-                <ServiceCard
-                  {...service}
-                  setActiveService={setActiveService}
-                  setPrev={setPrev}
-                  activeService={activeService}
-                />
+            {services.slice(0, 4).map((service) => (
+              <motion.div key={service.title} className="h-[250px] flex-1" initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: service.idx / 20 }}>
+                <ServiceCard {...service} setActiveService={setActiveService} setPrev={setPrev} activeService={activeService} />
                 {/* setActiveService={setActiveService} setPrev={setPrev} activeService={activeService} */}
               </motion.div>
             ))}
           </div>
           <Collapse isOpened={activeService && activeService <= 4}>
-            <div
-              className={`bg-black text-white dark:bg-white dark:text-black p-8 font-heading hidden lg:block`}
-            >
-              {services[activeService - 1]?.description ||
-                services[prev - 1]?.description}
-            </div>
+            <div className={`bg-black text-white dark:bg-white dark:text-black p-8 font-heading hidden lg:block`}>{services[activeService - 1]?.description || services[prev - 1]?.description}</div>
           </Collapse>
 
           <div className="flex justify-center">
-            {services.slice(3).map((service) => (
-              <motion.div
-                key={service.title}
-                className="h-[250px] flex-1"
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: service.idx / 20 }}
-              >
-                <ServiceCard
-                  {...service}
-                  setActiveService={setActiveService}
-                  activeService={activeService}
-                  setPrev={setPrev}
-                />
+            {services.slice(4).map((service) => (
+              <motion.div key={service.title} className="h-[250px] flex-1" initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: service.idx / 20 }}>
+                <ServiceCard {...service} setActiveService={setActiveService} activeService={activeService} setPrev={setPrev} />
               </motion.div>
             ))}
           </div>
           <Collapse isOpened={activeService && activeService > 4}>
-            <div
-              className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden lg:block`}
-            >
-              {services[activeService - 1]?.description ||
-                services[prev - 1]?.description}
-            </div>
+            <div className={`bg-black dark:bg-white dark:text-black text-white p-8 font-heading hidden lg:block`}>{services[activeService - 1]?.description || services[prev - 1]?.description}</div>
           </Collapse>
         </div>
       </div>
       {!isServicesPage && (
         <div className="flex justify-center">
-          <Button2 to={'/services'}>MEHR LESEN</Button2>
+          <Button2 to={"/services"}>MEHR LESEN</Button2>
         </div>
       )}
     </Section>
-  );
-};
+  )
+}
